e694c41f2342cc835d867465fc008d0b129f88d4
liramsalem <liramsalem@gmail.com>
2022-06-05 09:42:26 +0300

    4

--- utilities/db/db_UpdateProfile.py
+++ utilities/db/db_UpdateProfile.py
@@ -2,48 +2,57 @@ from utilities.db.db_manager import dbManager
 
 class DBupdate_profile:
 
-    def change_nickname(self, nickname, user_id):
-        query = "update tremplus_db.users set nickname='%s' where user_id='%s';" % (nickname, user_id)
+    @staticmethod
+    def change_nickname(nickname, user_id):
+        query = "update users set nickname='%s' where user_id='%s';" % (nickname, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_email(self, user_email, user_id):
-        query = "update tremplus_db.users set user_email='%s' where user_id='%s';" % (user_email, user_id)
+    @staticmethod
+    def change_user_email(user_email, user_id):
+        query = "update users set user_email='%s' where user_id='%s';" % (user_email, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_phone(self, phone, user_id):
-        query = "update tremplus_db.users set phone='%s' where user_id='%s';" % (phone, user_id)
+    @staticmethod
+    def change_user_phone(phone, user_id):
+        query = "update users set phone='%s' where user_id='%s';" % (phone, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_kibutz(self, kibutz, user_id):
-        query = "update tremplus_db.users set kibutz='%s' where user_id='%s';" % (kibutz, user_id)
+    @staticmethod
+    def change_user_kibutz(kibutz, user_id):
+        query = "update users set kibutz='%s' where user_id='%s';" % (kibutz, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_profile_pic(self, profile_pic, user_id):
-        query = "update tremplus_db.users set profile_pic='%s' where user_id='%s';" % (profile_pic, user_id)
+    @staticmethod
+    def change_user_profile_pic(profile_pic, user_id):
+        query = "update users set profile_pic='%s' where user_id='%s';" % (profile_pic, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_license_plate(self, license_plate, user_id):
-        query = "update tremplus_db.drivers set license_plate='%s' where user_id='%s';" % (license_plate, user_id)
+    @staticmethod
+    def change_user_license_plate(license_plate, user_id):
+        query = "update drivers set license_plate='%s' where user_id='%s';" % (license_plate, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_car_company(self, car_company, user_id):
-        query = "update tremplus_db.drivers set car_company='%s' where user_id='%s';" % (car_company, user_id)
+    @staticmethod
+    def change_user_car_company(car_company, user_id):
+        query = "update drivers set car_company='%s' where user_id='%s';" % (car_company, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_car_color(self, car_color, user_id):
-        query = "update tremplus_db.drivers set car_color='%s' where user_id='%s';" % (car_color, user_id)
+    @staticmethod
+    def change_user_car_color(car_color, user_id):
+        query = "update drivers set car_color='%s' where user_id='%s';" % (car_color, user_id)
         dbManager.commit(query=query)
         return
 
-    def change_user_licence_driver_pic(self, licence_driver_pic, user_id):
-        query = "update tremplus_db.drivers set licence_driver_pic='%s' where user_id='%s';" % (licence_driver_pic, user_id)
+    @staticmethod
+    def change_user_licence_driver_pic(licence_driver_pic, user_id):
+        query = "update drivers set licence_driver_pic='%s' where user_id='%s';" % (licence_driver_pic, user_id)
         dbManager.commit(query=query)
         return
 
