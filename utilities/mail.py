cef6fe8e1560d1f60ed495b22c0debf5cb44ee0d
liramsalem <liramsalem@gmail.com>
2022-06-11 19:00:34 +0300

    5

--- utilities/mail.py
+++ utilities/mail.py
@@ -15,3 +15,38 @@ def sendEmail(fName,lName,email,body):
         smtp.login(msg["From"], "Tt123456789")
         smtp.send_message(msg)
     print(f"one message send to {fName} {lName}")
+
+#
+# def send(fName, lName, email, body):
+#     msg = EmailMessage()
+#     msg.set_content(f" שלום {fName} {lName} , \n\n {body} ")
+#     msg["Subject"] = "Tremplus"
+#     msg["From"] = "tremplus12@mail.com"
+#     msg["To"] = email
+#
+#     context = ssl.create_default_context()
+#
+#     with smtplib.SMTP("smtp.mail.com", port=587) as smtp:
+#         smtp.starttls(context=context)
+#         print("k")
+#         smtp.login("tremplus12@mail.com", "SUCZB76PPJQCE4ADMQ2K")
+#         print("lir")
+#         smtp.send_message(msg)
+#     print(f"one message send to {fName} {lName}")
+#     send('fName', 'lName', 'liramsalem@gmail.com', "ffffff");
+
+# def send(fName, lName, email, body):
+#     msg = EmailMessage()
+#     msg.set_content(f" שלום {fName} {lName} , \n\n {body} ")
+#     msg["Subject"] = "Tremplus"
+#     msg["From"] = "tremplus@walla.co.il"
+#     msg["To"] = email
+#
+#     context = ssl.create_default_context()
+#
+#     with smtplib.SMTP("out.walla.co.il", port=587) as smtp:
+#         smtp.starttls(context=context)
+#         smtp.login(msg["From"], "Tremp12345678")
+#         smtp.send_message(msg)
+#     print(f"one message send to {fName} {lName}")
+
