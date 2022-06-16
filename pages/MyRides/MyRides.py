425383b946183b15ba7107310de7a859b9b47bf9
liramsalem <liramsalem@gmail.com>
2022-06-16 15:50:23 +0300

    5

--- pages/MyRides/MyRides.py
+++ pages/MyRides/MyRides.py
@@ -35,7 +35,7 @@ def index():
                 drive_details_for_email = db_Drives.find_drive_details(drive_id);
                 print(driver_details_for_email)
                 print(drive_details_for_email)
-                sendEmail(driver_details_for_email[1],driver_details_for_email[2],driver_details_for_email[0],f"נוסע הוסר מנסיעה החלה בתאריך {drive_details_for_email[2]} בשעה {drive_details_for_email[3]} מ- {drive_details_for_email[0]} ל- {drive_details_for_email[1]}")
+                # sendEmail(driver_details_for_email[1],driver_details_for_email[2],driver_details_for_email[0],f"נוסע הוסר מנסיעה החלה בתאריך {drive_details_for_email[2]} בשעה {drive_details_for_email[3]} מ- {drive_details_for_email[0]} ל- {drive_details_for_email[1]}")
                 return render_template('MyRides.html', message='ההסרה בוצעה בהצלחה!' , drivers_drive=drivers_drive, passenger_drive=passenger_drive)
         if request.form['req'] == "remove_drive":
             participents_details_for_email = db_Drives.find_participents_name_email(drive_id);
@@ -47,9 +47,9 @@ def index():
             db_Drives.remove_drive(drive_id);
             drivers_drive = db_Drives.find_all_drivers_drives(session['user_id'])
             passenger_drive = db_Drives.find_all_passenger_drives(session['user_id'])
-            for i in range(len(participents_details_for_email)):
-                sendEmail(participents_details_for_email[i][1], participents_details_for_email[i][2], participents_details_for_email[i][0],
-                      f"שימ/י לב!, הנסיעה מ-  {drive_details_for_email[0]} ל- {drive_details_for_email[1]} ב- {drive_details_for_email[2]} בשעה {drive_details_for_email[1]} התבטלה. ")
+            # for i in range(len(participents_details_for_email)):
+            #     sendEmail(participents_details_for_email[i][1], participents_details_for_email[i][2], participents_details_for_email[i][0],
+            #           f"שימ/י לב!, הנסיעה מ-  {drive_details_for_email[0]} ל- {drive_details_for_email[1]} ב- {drive_details_for_email[2]} בשעה {drive_details_for_email[3]} התבטלה. ")
             return render_template('MyRides.html', message='ההסרה בוצעה בהצלחה!', drivers_drive=drivers_drive,
                                    passenger_drive=passenger_drive)
     return render_template('MyRides.html',drivers_drive=drivers_drive,passenger_drive=passenger_drive)
