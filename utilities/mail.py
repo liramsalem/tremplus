8f60ef5dccd4a66c7f96671c2731fa7d5afc72d6
liramsalem <liramsalem@gmail.com>
2022-06-19 19:29:47 +0300

    li

--- utilities/mail.py
+++ utilities/mail.py
@@ -5,14 +5,14 @@ def sendEmail(fName,lName,email,body):
     msg = EmailMessage()
     msg.set_content(f" שלום {fName} {lName} , \n\n {body} ")
     msg["Subject"] = "Tremplus"
-    msg["From"] = "tremplus@outlook.com"
+    msg["From"] = "tremplus6@gmail.com"
     msg["To"] = email
 
     context = ssl.create_default_context()
 
-    with smtplib.SMTP("smtp.outlook.com", port=587) as smtp:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as smtp:
         smtp.starttls(context=context)
-        smtp.login(msg["From"], "Tt123456789")
+        smtp.login(msg["From"], "ffpfspjjkqkistii")
         smtp.send_message(msg)
     print(f"one message send to {fName} {lName}")
 
